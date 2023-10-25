430d345e8d4d86fe3b8ac6af6b2e838c07983fce
Roman <origami028@gmail.com>
2023-10-25 19:11:38 +0900

    first commit

---
+++ src/Hourly.java
@@ -0,0 +1,18 @@
+public class Hourly {
+}
+/** Класс работника с почасовой оплатой труда
+ *
+ */
+class HourlyWorker extends Worker {
+    private double hourlyRate;
+
+    public HourlyWorker(String name, double hourlyRate) {
+        super(name);
+        this.hourlyRate = hourlyRate;
+    }
+
+    @Override
+    public double calculateAverageSalary() {
+        return 20.8 * 8 * hourlyRate;
+    }
+}
